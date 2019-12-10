b48c489bdee0b19e007158a930c03a3f1cfb5e7e
floodlock <floodlockgames9@gmail.com>
2019-12-10 13:29:45 +0000

    clearing out asuna.eris

--- commands/connections.js
+++
@@ -1,8 +0,0 @@
-    
-exports.run = (client, msg) => {
-        msg.channel.send(`:notes: Currently playing some music in **${client.voice.connections.size}** voice channels.`);
-}
-exports.help = {
-  name: 'connections',
-  aliases: []
-};
